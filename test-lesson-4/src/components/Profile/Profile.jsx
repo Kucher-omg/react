7a3d6bfef5b541a5e556f74432d1a4ab5079f7b3
Vlad <vladkycherenko98@gmail.com>
2020-10-28 18:38:13 +0200

    Lesson_60_WithRouter_ProfilePage_begin#2

--- test-lesson-4/src/components/Profile/Profile.jsx
+++ test-lesson-4/src/components/Profile/Profile.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import ProfileInfo from './ProfileInfo/ProfileInfo';
 import MyPostContainer from './MyPost/MyPostContainer';
-import Preloader from '../common/Preloader/Preloader';
 
 
 
 const Profile = (props) => {
-  if (!props.profile) {
-    return (
-      <Preloader />
-    );
-  }
+
   return (
     <div >
 
--- test-lesson-4/src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ test-lesson-4/src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import classes from './ProfileInfo.module.css';
 import MyPost from '../MyPost/MyPost';
-
+import Preloader from '../../common/Preloader/Preloader';
+import styles from './ProfileInfo.module.css';
 
 
 const ProfileInfo = (props) => {
+  if (!props.profile) {
+    return (
+      <Preloader />
+    );
+  }
+
+
   return (
     <div>
       <div>
@@ -12,8 +20,13 @@ const ProfileInfo = (props) => {
       </div>
 
       <div className={classes.description_block}>
-        <img src={props.profile.photos.small} alt="" />
-        <span>{props.profile.lookingForAJobDescription}</span>
+        <img className={styles.photo}
+          src={props.profile.photos.small != null
+            ? props.profile.photos.small
+            : 'https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg'} />
+        {/* <img src={props.profile.photos.small} alt="" /> */}
+        <span>{props.profile.fullName}</span>
+        <div>{props.profile.lookingForAJobDescription}</div>
       </div>
     </div>
   );
